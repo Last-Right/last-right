6414d0940937c84318224a51b200c78e7f33dfe4
Amirka-Kh <amirabikhuja@gmail.com>
2023-06-27 11:31:50 +0300

    Add bad request handling and VideoDetail view

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name = 'last-right'
 
 urlpatterns = [
-    # 'api/videos'
-    path('', views.VideoList.as_view(), name='video-list'),
+    path('', views.BadRequestView.as_view(), name='bad_request'),
+    path('api/videos', views.VideoList.as_view(), name='video-list'),
     # path('api/videos/<int:video_id>', views.VideoDetail.as_view(), name='video-detail'),
-    # path('api/videos/<int:video_id>/related', views.RelatedVideos.as_view(), name='related-videos'),
     # path('api/videos/<int:video_id>/comments', views.VideoComments.as_view(), name='video-comments'),
     # path('api/search', views.VideoSearch.as_view(), name='video-search'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,13 @@ from .models import VideoModel
 from .serializers import VideoModelSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.views.generic import TemplateView
+from django.http import HttpResponseBadRequest
+
+
+class BadRequestView(TemplateView):
+    def dispatch(self, request, *args, **kwargs):
+        return HttpResponseBadRequest()
 
 
 class VideoList(APIView):
@@ -13,8 +20,21 @@ class VideoList(APIView):
         # videos = YourVideoModel.objects.all()
         videos = VideoModel.objects.filter(tags__name__in=tags)
 
-        # Serialize the videos data if needed
+        # Serialize the videos' data if needed
         serializer = VideoModelSerializer(videos, many=True)
+        serialized_data = serializer.data
+
+        # Return the serialized data as a response
+        return Response(serialized_data)
+
+
+class VideoDetail(APIView):
+    def get(self, request, video_id):
+        # Use the video_id parameter in your logic
+        video = VideoModel.objects.filter(id=video_id)
+
+        # Serialize the video data if needed
+        serializer = VideoModelSerializer(video)
         serialized_data = serializer.data
 
         # Return the serialized data as a response
